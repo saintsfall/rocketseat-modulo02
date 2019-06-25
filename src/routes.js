60c7d8aa474116fe100ba6adc6c54464b381fd3c
Caio Santos <caio.f.santos@icloud.com>
2019-06-25 11:38:30 -0300

    Files and configs regarding format created/setted up

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import Router from "express";
+import Router from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World from the home page" });
-});
+routes.get('/', (req, res) =>
+  res.json({ message: 'Hello World from the home page' })
+);
 
 export default routes;
