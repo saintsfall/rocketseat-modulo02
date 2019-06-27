01a4ca3d25121802313b551be08e02b2b5e1c0c7
Caio Santos <caio.f.santos@icloud.com>
2019-06-27 16:17:44 -0300

    Authentication added, jwt added

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '857ef7664650fe3d295f71f4276e27cb',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
@@ -13,5 +14,6 @@ const routes = new Router();
 // });
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
