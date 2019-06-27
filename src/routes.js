ab4a9121d631cb97b1fbdf4f8c3439654994b6b4
Caio Santos <caio.f.santos@icloud.com>
2019-06-27 13:04:28 -0300

    Config file for data base and routes changed

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({ message: 'Hello World from the home page' })
-);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Caio Santos',
+    email: 'caio.f.santos@icloud.com',
+    password_hash: '1234',
+  });
+  res.json(user);
+});
 
 export default routes;
