81e4abce045e62288660c45a243a93b1f25aa76b
Caio Santos <caio.f.santos@icloud.com>
2019-06-27 15:44:27 -0300

    Minor fixes, added bcrypt and UserController

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
-import Router from 'express';
-import User from './app/models/User';
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Caio Santos',
-    email: 'caio.f.santos@icloud.com',
-    password_hash: '1234',
-  });
-  res.json(user);
-});
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'Caio Santos',
+//     email: 'caio.f.santos@icloud.com',
+//     password_hash: '1234',
+//   });
+//   res.json(user);
+// });
+
+routes.post('/users', UserController.store);
 
 export default routes;
